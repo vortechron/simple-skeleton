8934b41e9b597571ddf8a6d3fe7bb3469b11793e
Amirul Adli <sayaamiruladli@gmail.com>
2018-11-20 10:39:34 +0800

    fix bug

--- partials/footer.php
+++ partials/footer.php
@@ -1,5 +1,5 @@
 
-    <script src="js/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="js/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="js/jquery-3.3.1.slim.min.js"></script>
+    <script src="js/popper.min.js"></script>
 </body>
 </html>
